bcdbcb3c27ec66c170f402f0aaf47547186889d2
mirko-san <41379069+mirko-san@users.noreply.github.com>
2020-07-20 23:49:54 +0900

    [Change] 子コンポーネントに切り出し

--- resume/src/components/Skill.vue
+++ resume/src/components/Skill.vue
@@ -2,118 +2,52 @@
   <div>
     <h2>Skill</h2>
     <h3>JavaScript</h3>
-    <ul>
-      <li v-for="item in skills.JavaScript" :key="item.name">
-        <dl>
-          <dt>{{ item.name }}</dt>
-          <dd>
-            <div>
-              <span>business experience</span>
-              <span>{{ item.businessExperience }}</span>
-            </div>
-            <p>{{ item.comment }}</p>
-            <div v-if="item.sampleSource">
-              <span>sample source code</span>
-              <span>
-                <a :href="item.sampleSource" target="_blank">
-                  {{ item.sampleSource }}
-                </a>
-              </span>
-            </div>
-          </dd>
-        </dl>
-      </li>
-    </ul>
+    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+      <div v-for="item in skills.JavaScript" :key="item.name">
+        <SkillCard :item="item"></SkillCard>
+      </div>
+    </div>
 
     <h3>HTML</h3>
-    <dl>
-      <dt>HTML</dt>
-      <dd>
-        <div>
-          <span>business experience</span>
-          <span>{{ skills.HTML.businessExperience }}</span>
-        </div>
-        <p>{{ skills.HTML.comment }}</p>
-      </dd>
-    </dl>
+    <SkillCard :item="skills.HTML"></SkillCard>
 
     <h3>CSS</h3>
-    <dl>
-      <dt>CSS</dt>
-      <dd>
-        <div>
-          <span>business experience</span>
-          <span>{{ skills.CSS.businessExperience }}</span>
-        </div>
-        <p>{{ skills.CSS.comment }}</p>
-      </dd>
-    </dl>
+    <SkillCard :item="skills.CSS"></SkillCard>
 
     <h3>Python</h3>
-    <dl>
-      <dt>Python</dt>
-      <dd>
-        <div>
-          <span>business experience</span>
-          <span>{{ skills.Python.businessExperience }}</span>
-        </div>
-        <p>{{ skills.Python.comment }}</p>
-      </dd>
-    </dl>
+    <SkillCard :item="skills.Python"></SkillCard>
 
     <h3>AWS</h3>
-    <ul>
-      <li v-for="item in skills.AWS" :key="item.name">
-        <dl>
-          <dt>{{ item.name }}</dt>
-          <dd>
-            <div>
-              <span>business experience</span>
-              <span>{{ item.businessExperience }}</span>
-            </div>
-            <p>{{ item.comment }}</p>
-            <div v-if="item.sampleSource">
-              <span>sample source code</span>
-              <span>
-                <a :href="item.sampleSource" target="_blank">
-                  {{ item.sampleSource }}
-                </a>
-              </span>
-            </div>
-          </dd>
-        </dl>
-      </li>
-    </ul>
+    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+      <div v-for="item in skills.AWS" :key="item.name">
+        <SkillCard :item="item"></SkillCard>
+      </div>
+    </div>
 
     <h3>os</h3>
-    <ul>
+    <ul class="uk-list uk-list-disc">
       <li v-for="(value, name, index) in skills.os" :key="index">
         {{ value }}
       </li>
     </ul>
 
     <h3>etc</h3>
-    <ul>
-      <li v-for="item in skills.etc" :key="item.name">
-        <dl>
-          <dt>{{ item.name }}</dt>
-          <dd>
-            <div>
-              <span>business experience</span>
-              <span>{{ item.businessExperience }}</span>
-            </div>
-            <p>{{ item.comment }}</p>
-          </dd>
-        </dl>
-      </li>
-    </ul>
+    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+      <div v-for="item in skills.etc" :key="item.name">
+        <SkillCard :item="item"></SkillCard>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SkillCard from './SkillCard.vue';
 import skillJson from '@/assets/dist/json/skill.json';
 
 export default {
+  components: {
+    SkillCard
+  },
   data: function() {
     return {
       skills: skillJson.skill
---
+++ resume/src/components/SkillCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="uk-card uk-card-default uk-card-body">
+    <h3 class="uk-card-title">{{ item.name }}</h3>
+    <div>
+      <span>business experience</span>
+      <span v-if="item.businessExperience" class="uk-label uk-label-success">
+        {{ item.businessExperience }}
+      </span>
+      <span v-else class="uk-label uk-label-danger">
+        {{ item.businessExperience }}
+      </span>
+    </div>
+    <p class="uk-text-left">{{ item.comment }}</p>
+    <div v-if="item.sampleSource">
+      <span>sample source code</span>
+      <span>
+        <a :href="item.sampleSource" target="_blank">
+          {{ item.sampleSource }}
+        </a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillCard',
+  props: {
+    item: Object
+  }
+};
+</script>
