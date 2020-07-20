2da79f0239fb9acf9e7e0d9e462090977a4283f9
mirko-san <41379069+mirko-san@users.noreply.github.com>
2020-07-20 22:35:16 +0900

    [Add] Skill.vue

---
+++ resume/src/components/Skill.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>It is skill.vue</p>
+  </div>
+</template>
+
+<script>
+import skillJson from '@/assets/dist/json/skill.json';
+
+export default {
+  data: function() {
+    return {
+      skills: skillJson.skill
+    };
+  },
+  created() {
+    console.log('hoge');
+    console.log(skillJson);
+  }
+};
+</script>
