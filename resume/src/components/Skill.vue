f7e247965017b998e37e262cc378c14a4aa3f043
mirko-san <41379069+mirko-san@users.noreply.github.com>
2020-07-20 23:06:03 +0900

    [Change] Skill.vueのマークアップとそれに伴う変更

--- resume/src/components/Skill.vue
+++ resume/src/components/Skill.vue
@@ -1,6 +1,112 @@
 <template>
   <div>
-    <p>It is skill.vue</p>
+    <h2>Skill</h2>
+    <h3>JavaScript</h3>
+    <ul>
+      <li v-for="item in skills.JavaScript" :key="item.name">
+        <dl>
+          <dt>{{ item.name }}</dt>
+          <dd>
+            <div>
+              <span>business experience</span>
+              <span>{{ item.businessExperience }}</span>
+            </div>
+            <p>{{ item.comment }}</p>
+            <div v-if="item.sampleSource">
+              <span>sample source code</span>
+              <span>
+                <a :href="item.sampleSource" target="_blank">
+                  {{ item.sampleSource }}
+                </a>
+              </span>
+            </div>
+          </dd>
+        </dl>
+      </li>
+    </ul>
+
+    <h3>HTML</h3>
+    <dl>
+      <dt>HTML</dt>
+      <dd>
+        <div>
+          <span>business experience</span>
+          <span>{{ skills.HTML.businessExperience }}</span>
+        </div>
+        <p>{{ skills.HTML.comment }}</p>
+      </dd>
+    </dl>
+
+    <h3>CSS</h3>
+    <dl>
+      <dt>CSS</dt>
+      <dd>
+        <div>
+          <span>business experience</span>
+          <span>{{ skills.CSS.businessExperience }}</span>
+        </div>
+        <p>{{ skills.CSS.comment }}</p>
+      </dd>
+    </dl>
+
+    <h3>Python</h3>
+    <dl>
+      <dt>Python</dt>
+      <dd>
+        <div>
+          <span>business experience</span>
+          <span>{{ skills.Python.businessExperience }}</span>
+        </div>
+        <p>{{ skills.Python.comment }}</p>
+      </dd>
+    </dl>
+
+    <h3>AWS</h3>
+    <ul>
+      <li v-for="item in skills.AWS" :key="item.name">
+        <dl>
+          <dt>{{ item.name }}</dt>
+          <dd>
+            <div>
+              <span>business experience</span>
+              <span>{{ item.businessExperience }}</span>
+            </div>
+            <p>{{ item.comment }}</p>
+            <div v-if="item.sampleSource">
+              <span>sample source code</span>
+              <span>
+                <a :href="item.sampleSource" target="_blank">
+                  {{ item.sampleSource }}
+                </a>
+              </span>
+            </div>
+          </dd>
+        </dl>
+      </li>
+    </ul>
+
+    <h3>os</h3>
+    <ul>
+      <li v-for="(value, name, index) in skills.os" :key="index">
+        {{ value }}
+      </li>
+    </ul>
+
+    <h3>etc</h3>
+    <ul>
+      <li v-for="item in skills.etc" :key="item.name">
+        <dl>
+          <dt>{{ item.name }}</dt>
+          <dd>
+            <div>
+              <span>business experience</span>
+              <span>{{ item.businessExperience }}</span>
+            </div>
+            <p>{{ item.comment }}</p>
+          </dd>
+        </dl>
+      </li>
+    </ul>
   </div>
 </template>
 
