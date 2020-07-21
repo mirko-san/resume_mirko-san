13c4addf63ee0bf4d71684ee59cf36e69f4f9593
mirko-san <41379069+mirko-san@users.noreply.github.com>
2020-07-21 14:35:48 +0900

    [Change] App.vue 不要コードのコメントアウト

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <img
+  <div class="uk-container">
+    <!-- <img
       alt="Vue logo"
       src="./assets/logo.png"
-    />
+    /> -->
     <Skill />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
@@ -21,14 +21,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
