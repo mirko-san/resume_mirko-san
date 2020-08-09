481e5a0d9b7f126be37db1a6a519a27beda4878a
mirko-san <41379069+mirko-san@users.noreply.github.com>
2020-08-10 00:58:49 +0900

    [Change] ymlの記述統一にともない、templateを簡略化

--- resume/src/components/Skill.vue
+++ resume/src/components/Skill.vue
@@ -1,52 +1,12 @@
 <template>
   <div>
     <h2>Skill</h2>
-    <h3>JavaScript</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.JavaScript" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>HTML</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.HTML" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>CSS</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.CSS" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>Python</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.Python" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>AWS</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.AWS" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>os</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.os" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
-      </div>
-    </div>
-
-    <h3>etc</h3>
-    <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div v-for="item in skills.etc" :key="item.name">
-        <SkillCard :item="item"></SkillCard>
+    <div class="uk-margin-large-bottom" v-for="item in Object.keys(skills)" :key="item">
+      <h3>{{ item }}</h3>
+      <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+        <div v-for="itemName in skills[item]" :key="itemName.name">
+          <SkillCard :item="itemName"></SkillCard>
+        </div>
       </div>
     </div>
   </div>
@@ -66,8 +26,6 @@ export default {
     };
   },
   created() {
-    console.log('hoge');
-    console.log(skillJson);
   }
 };
 </script>
